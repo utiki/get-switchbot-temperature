71e477d22e6915a68c3a90a9b4bd1541daefa12e
enigma <the.world.truth3616@gmail.com>
2025-01-22 04:19:19 +0900

    Change to sync processing and cache only

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -7,6 +7,10 @@ sys.path.append(str(Path(__file__).parent.parent / "db"))
 from database import get_temperatures_by_latest, get_weather_by_date
 import models
 from fastapi.middleware.cors import CORSMiddleware
+from redis.asyncio import Redis
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
 
 app = FastAPI()
 
@@ -24,11 +28,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
+@app.on_event("startup")
+async def startup():
+    redis = Redis(host="localhost", port=6379)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+    
+    
 @app.get("/temperature")
-def read_root():
+@cache(expire=60) 
+def get_temperature():
     return get_temperatures_by_latest()
 
+
 @app.get("/weather")
-def read_root():
+@cache(expire=60) 
+def get_weather():
     return get_weather_by_date(date.today())
